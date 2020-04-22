666e1ff401fb3e9fca9d32fca766d9b1000609cd
garret.valentino@gmail.com <garret.valentino@gmail.com>
2020-04-22 16:40:29 -0500

    this is after i have done everything within the PDF

--- TrashCollector2/Startup.cs
+++ TrashCollector2/Startup.cs
@@ -12,6 +12,9 @@ using TrashCollector2.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+using TrashCollector2.ActionFilters;
 
 namespace TrashCollector2
 {
@@ -34,6 +37,11 @@ namespace TrashCollector2
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultUI()
                 .AddDefaultTokenProviders();
+            services.AddScoped<ClaimsPrincipal>(s => s.GetService<IHttpContextAccessor>().HttpContext.User);
+            services.AddControllers(config =>
+            {
+                config.Filters.Add(typeof(GlobalRouting));
+            });
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
