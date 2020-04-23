ff28f7cede32a755919488b2458c4c3892f040a3
garret.valentino@gmail.com <garret.valentino@gmail.com>
2020-04-23 09:27:04 -0500

    this is with the employee class done

---
+++ TrashCollector2/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrashCollector2.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public int EmployeeNumber { get; set; }
+        public string Name { get; set; }
+        public int EmployeeZipCode { get; set; }
+
+        [ForeignKey("IdentityEmployee")]
+        public string IdentityEmployeeId { get; set; }
+        public IdentityUser IdentityEmployee { get; set; }
+
+    }
+}
